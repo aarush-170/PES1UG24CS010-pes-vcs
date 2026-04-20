8628c85073b7d756583a7238bed00fe0676ec4ff
aarush-170 <aarush17069@gmail.com>
2026-04-20 08:11:45 +0000

    phase4: add author timestamp and commit message

--- commit.c
+++ commit.c
@@ -204,7 +204,7 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         return -1;
     }
 
-    // read previous commit
+    // parent commit
     ObjectID parent_id;
 
     if (head_read(&parent_id) == 0) {
@@ -213,6 +213,15 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     } else {
         commit.has_parent = 0;
     }
+
+    // author name
+    snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
+
+    // timestamp
+    commit.timestamp = (uint64_t)time(NULL);
+
+    // commit message
+    snprintf(commit.message, sizeof(commit.message), "%s", message);
 
     return 0;
 }
